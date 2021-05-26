df3ca83b492a99380f5dc63c74d10109a22e1c4d
SDarKnight <stepan.y.kravchenko@gmail.com>
2021-05-26 16:36:34 +1000

    + Rotate

--- Lesson 1 Tetris.cpp
+++ Lesson 1 Tetris.cpp
@@ -7,34 +7,26 @@
 
 using namespace std;
 
-char figure[7][4][5] = {
-	{
-		"OO",  // 0
-		"OO"
-	},
-	{
-		"O  ", // 1
-		"OOO"
-	},
-	{
-		"  O", // 2
-		"OOO"
-	},
-	{
-		"OOOO" // 3
-	},
-	{
-		" O ", // 4
-		"OOO"
-	},
-	{
-		" OO", // 5
-		"OO "
-	},
-	{
-		"OO ", // 6
-		" OO"
-	}
+char figure[7][4][4][5] = {
+	{ {"OO","OO"}, {"OO","OO"}, {"OO","OO"}, {"OO","OO"} },					// OO	0
+																			// OO
+
+	{ {"O  ","OOO"}, {"OO","O ","O "}, {"OOO","  O"}, {" O"," O","OO"} },	// O	1
+																			// OOO
+
+	{ {"  O","OOO"}, {"O ","O ","OO"}, {"OOO","O  "}, {"OO"," O"," O"} },	//   O	2
+																			// OOO	
+
+	{ {	"OOOO"}, {"O","O","O","O"}, {"OOOO"}, {"O","O","O","O"} },			// OOOO	3
+
+	{ {" O ","OOO"}, {"O ","OO","O "}, {"OOO"," O "}, {" O","OO"," O"} },	//  O	4
+																			// OOO
+
+	{ {" OO","OO "}, {"O ","OO"," O"}, {" OO","OO "}, {"O ","OO"," O"} },	//  OO	5
+																			// OO 
+	
+	{ {"OO "," OO"}, {" O","OO","O "}, {"OO "," OO"}, {" O","OO","O "} }	// OO	6
+																			//  OO
 };
 
 char screen[10][13] = {
@@ -46,7 +38,7 @@ char screen[10][13] = {
 	"|          |",
 	"|          |",
 	"|          |",
-	"|          |",
+	"| O        |",
 	"------------"
 };
 
@@ -61,11 +53,11 @@ void DrawScreen()
 	}
 }
 
-void DrawFigure(int n, short x, short y)
+void DrawFigure(int n, int rot, short x, short y)
 {
-	for (short r = 0; r < 2; r++) {
+	for (short r = 0; r < 4 && figure[n][rot][r][0]; r++) {
 		SetConsoleCursorPosition(out, { short(11 + x), short(r + y) } );
-		auto f = figure[n][r];
+		auto f = figure[n][rot][r];
 		WriteConsoleA(out, f, strlen(f), 0, 0);
 		//cout << "\t\t" << screen[i] << '\n';
 	}
@@ -90,30 +82,44 @@ void CursorOff()
 int main()
 {
 	CursorOff();
-	//srand((int)time(0));
+	srand((int)time(0));
 	//for (int i = 0; i < 60; ++i) {
-	//	//cout << rand() % 100 << ' ';
+	//	//cout <<  << ' ';
 	//	DrawScreen(i);
 	//	Sleep(50);
 	//	ClearScreen(i);
 	//}
 	//DrawScreen();
-	int x = 0, y = 0;
-	int f = 6;
+	int f = rand() % 7;
+	int rot = 0;
+	int len = strlen(figure[f][rot][0]);
+	int x = len == 2 ? 4 : 3, y = 0;
+	int i = 0;
 	while (GetAsyncKeyState(VK_ESCAPE) >= 0) {
+		len = strlen(figure[f][rot][0]);
+		int h = !figure[f][rot][1][0] ? 1 :
+			    !figure[f][rot][2][0] ? 2 :
+				!figure[f][rot][3][0] ? 3 :
+										4 ;
 		if (GetAsyncKeyState(VK_LEFT) < 0) x--;
 		if (GetAsyncKeyState(VK_RIGHT) < 0) x++;
-		x = clamp(x, 0, 10 - (int)strlen(figure[f][0]));
-		if (GetAsyncKeyState(VK_UP) < 0) y--;
+		x = clamp(x, 0, 10 - len);
+		if (GetAsyncKeyState(VK_UP) & 1) rot++;
+		if (GetAsyncKeyState('C') & 1) rot--;
+		rot &= 3;
+
 		if (GetAsyncKeyState(VK_DOWN) < 0) y++;
-		y = clamp(y, 0, 9 - (figure[f][1][0] ? 2 : 1) );
+		y = clamp(y, 0, 9 - h);
 		ClearScreen();
 		DrawScreen();
-		DrawFigure(f, x, y);
+		DrawFigure(f, rot, x, y);
 		Sleep(16);
+		if( i % 10 == 9) y++;
+		i++;
 	}
 	
 	SetConsoleCursorPosition(out, { 0,15 });
 	WriteConsoleA(out, "\n", 1, 0, 0);
     return 0;
+	
 }
